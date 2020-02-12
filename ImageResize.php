b2272fae4530a500e520974cdfec2395b3fab858
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-02-12 13:18:58 +0400

    Give `$extensions` and `$imageWidth` parameters in ImageResize constructor

--- ImageResize.php
+++ ImageResize.php
@@ -5,10 +5,25 @@ namespace tc\bulkimageresize;
 class ImageResize
 {
     public $imagine;
+    /**
+     * Extensions to check each file agains before trying to resize
+     */
+    private $extensions = ['jpg', 'png', 'jpeg'];
 
-    public function __construct()
+    /**
+     * The image width which will be used to create thumbnails
+     */
+    private $imageWidth = 200; 
+
+    public function __construct($extensions = null, $imageWidth = null)
     {
         $this->imagine = new \Imagine\Gd\Imagine();
+        if ($extensions !== null) {
+            $this->extensions = $extensions;
+        }
+        if ($imageWidth !== null){
+            $this->imageWidth = $imageWidth;
+        }
     }
 
     public function resizeAllImages($dir)
@@ -20,9 +35,9 @@ class ImageResize
             if (!is_dir($path)) {
                 // Resize image
                 $ext = pathinfo($path, PATHINFO_EXTENSION);
-                if (in_array($ext, ['jpg', 'png', 'jpeg'])) {
+                if (in_array($ext, $this->extensions)) {
                     $this->imagine->open($path)
-                        ->thumbnail(new \Imagine\Image\Box(200, 200))
+                        ->thumbnail(new \Imagine\Image\Box($this->imageWidth, $this->imageWidth))
                         ->save($path);
                 }
             } else if ($value != "." && $value != "..") {
